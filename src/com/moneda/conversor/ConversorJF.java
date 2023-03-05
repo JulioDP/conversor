220905658ed4e9c37c4c2d9fd9f6343505cc7d5f
Julio Dominguez <chiapasjeremai@gmail.com>
2023-03-05 15:01:01 -0600

    Agregando la parte de temperatura y creadno un jcombox

--- src/com/moneda/conversor/ConversorJF.java
+++ src/com/moneda/conversor/ConversorJF.java
@@ -12,6 +12,8 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 
 public class ConversorJF extends JFrame {
@@ -19,6 +21,9 @@ public class ConversorJF extends JFrame {
 	private JPanel contentPane;
 	private JTextField total;
 	private JTextField valor;
+	String moneda = "Monedas";
+	String temperatura = "Temperatura";
+	Unidad ins = null;
 
 	/**
 	 * Launch the application.
@@ -40,6 +45,7 @@ public class ConversorJF extends JFrame {
 	 * Create the frame.
 	 */
 	public ConversorJF() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 500);
 		contentPane = new JPanel();
@@ -49,24 +55,58 @@ public class ConversorJF extends JFrame {
 		
 		JLabel conversor = new JLabel("Conversor");
 		conversor.setFont(new Font("Tw Cen MT", Font.BOLD, 43));
-		conversor.setBounds(410, 93, 182, 96);
+		conversor.setBounds(411, 21, 182, 96);
 		contentPane.add(conversor);
 		
 		JComboBox listaConversion = new JComboBox();
 		listaConversion.setFont(new Font("Tahoma", Font.BOLD, 14));
-		listaConversion.setModel(new DefaultComboBoxModel(new String[] {"De Peso MXN a Dolar", "De Peso MXN a Euro", "De Peso MXN a Libras Esterlinas", "Peso MXN a Yen", "Peso MXN a Won"}));
+		
+		JComboBox listaopcion = new JComboBox();
+		listaopcion.setFont(new Font("Tahoma", Font.BOLD, 14));
+		listaopcion.setModel(new DefaultComboBoxModel(new String[] {"Monedas", "Temperatura"}));
+		listaopcion.setBounds(378, 134, 201, 43);
+		contentPane.add(listaopcion);
+		
+		
+		
+		
+		listaopcion.addItemListener(new ItemListener() {
+			String objeto[];
+			DefaultComboBoxModel mo = null;
+			  public void itemStateChanged(ItemEvent itemEvent) {
+				  if (moneda.equals(listaopcion.getSelectedItem().toString())) {
+					  	ins = new Moneda();
+						objeto = new String[]
+								{"De Peso MXN a Dolar", "De Peso MXN a Euro", "De Peso MXN a Libras Esterlinas", "Peso MXN a Yen", "Peso MXN a Won"};
+						mo = new DefaultComboBoxModel(objeto);
+					}else {
+						ins = new Temperatura();
+						objeto = new String[]
+								{"De Celsius a Kelvin", "De Kelvin a Celsius", "De Celsius a Fahrenheit", "De Fahrenheit a Celsius", "De Kelvin a Fahrenheit","De Fahrenheit a Kelvin"};
+						mo = new DefaultComboBoxModel(objeto);
+					}
+				  listaConversion.setModel(mo);
+			  }
+			});
+		
+		
+	
+		
+		
+		
+		
 		listaConversion.setBounds(493, 221, 201, 43);
 		contentPane.add(listaConversion);
 		
 		total = new JTextField();
 		total.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		total.setBounds(493, 287, 116, 43);
+		total.setBounds(493, 287, 206, 43);
 		contentPane.add(total);
 		total.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Valor");
 		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
-		lblNewLabel.setBounds(187, 237, 64, 27);
+		lblNewLabel.setBounds(258, 229, 64, 27);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Total");
@@ -77,24 +117,25 @@ public class ConversorJF extends JFrame {
 		valor = new JTextField();
 		valor.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		valor.setColumns(10);
-		valor.setBounds(250, 220, 116, 44);
+		valor.setBounds(337, 220, 116, 44);
 		contentPane.add(valor);
 		
 		JButton btnNewButton = new JButton("Convertir");
 		btnNewButton.setFont(new Font("Tw Cen MT", Font.BOLD, 20));
+		
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				Moneda moneda = new Moneda();
-				moneda.setValor(Double.parseDouble(valor.getText()));
-				moneda.convertir(moneda, listaConversion.getSelectedItem().toString());
-				total.setText(moneda.getCantidad()+"");
+				Convertible con = ins;
+				ins.setValor(Double.parseDouble(valor.getText()));
+				con.convertir(ins, listaConversion.getSelectedItem().toString());
+				total.setText(ins.getCantidad()+"");
 				
 				
 				
 			}
 		});
-		btnNewButton.setBounds(250, 307, 116, 23);
+		btnNewButton.setBounds(250, 299, 122, 31);
 		contentPane.add(btnNewButton);
 		
 	}
