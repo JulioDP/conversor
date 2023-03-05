a132b0ae952d515f4a966669166b5a9799862b3d
Julio Dominguez <chiapasjeremai@gmail.com>
2023-03-04 23:51:43 -0600

    probando la funcionalidad de la aplicacion

---
+++ src/com/moneda/conversor/testConversor.java
@@ -0,0 +1,14 @@
+package com.moneda.conversor;
+
+public class testConversor {
+	
+	public static void main(String[] args) {
+		Moneda mo = new Moneda(20,200,"Peso MXN a Dolar");
+		
+		mo.convertir(mo, "Peso MXN a Won");
+	
+	}
+	
+		
+
+}
