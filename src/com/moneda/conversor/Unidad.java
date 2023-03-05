2c1559ca8c497e5a9b85b2277fc2402ae6c92688
Julio Dominguez <chiapasjeremai@gmail.com>
2023-03-04 18:16:18 -0600

    Dando formato a la unidad

--- src/com/moneda/conversor/Unidad.java
+++ src/com/moneda/conversor/Unidad.java
@@ -1,5 +1,49 @@
 package com.moneda.conversor;
 
-public class Unidad {
+public abstract class Unidad {
+	private double valor;
+	private double cantidad;
+	private String tipoConversion;
+	
+	public Unidad() {
+	
+	}
+	
+	
+	public Unidad(double valor, double cantidad) {
+		this.valor = valor;
+		this.cantidad = cantidad;
+	}
+	
 
+	public Unidad(double valor, double cantidad, String tipoConversion) {
+		this.valor = valor;
+		this.cantidad = cantidad;
+		this.tipoConversion = tipoConversion;
+	}
+
+
+	public double getValor() {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public double getCantidad() {
+		return cantidad;
+	}
+	
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public String getTipoConversion() {
+		return tipoConversion;
+	}
+	
+	public void setTipoConversion(String tipoConversion) {
+		this.tipoConversion = tipoConversion;
+	}	
 }
