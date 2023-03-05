31b400c5db2ebcb36c6761052478ba9c79a7a818
Julio Dominguez <chiapasjeremai@gmail.com>
2023-03-04 18:13:56 -0600

    Agregando los metodos getter y setter en la clase Moneda y implementando la interfaz convertible

--- src/com/moneda/conversor/Moneda.java
+++ src/com/moneda/conversor/Moneda.java
@@ -1,6 +1,48 @@
 package com.moneda.conversor;
 
-public class Moneda extends Unidad{
+public class Moneda extends Unidad implements Convertible{
+	
+	private String moneda;
+	
+
+	public Moneda() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Moneda(double valor, double cantidad, String tipoConversion) {
+		super(valor, cantidad, tipoConversion);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Moneda(double valor, double cantidad) {
+		super(valor, cantidad);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Moneda(double valor, double cantidad, String tipoConversion, String moneda) {
+		super(valor, cantidad, tipoConversion);
+		// TODO Auto-generated constructor stub
+		this.moneda = moneda;
+	}
+
+	@Override
+	public void convertir(Moneda moneda, String tipoConversion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getMoneda() {
+		return moneda;
+	}
+
+	public void setMoneda(String moneda) {
+		this.moneda = moneda;
+	}
+	
+	
+	
 	
 	
 	
